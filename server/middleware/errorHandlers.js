a100ab97266df97ff3ba55f2720893a32f5ae923
Viktoriia <pecourses>
2022-07-29 22:06:03 +0300

    Add middlewares to server

---
+++ server/middleware/errorHandlers.js
@@ -0,0 +1,8 @@
+module.exports.errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return;
+  }
+  res
+    .status(err?.status ?? 500)
+    .send({ errors: [{ title: err?.message ?? 'Internal server error' }] });
+};
