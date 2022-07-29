2e6db701394c55ea7641f4440e7d9431048ab938
Viktoriia <pecourses>
2022-07-29 21:30:03 +0300

    Add server base template

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,26 @@
+const { Message } = require('./../models');
+
+module.exports.getMessages = async (req, res, next) => {
+  const { limit = 20 } = req.query;
+
+  try {
+    const foundMessages = await Message.find()
+      .limit(limit)
+      .sort({ createdAt: -1 });
+    res.status(200).send({ data: foundMessages });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.createMessage = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const createdMessage = await Message.create(body);
+
+    res.status(201).send({ data: createdMessage });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ server/models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const env = process.env.NODE_ENV ?? 'development';
+const { host, port, dbName } = require('./../configs/mongoDBConfig.json')[env];
+
+mongoose
+  .connect(`mongodb://${host}:${port}/${dbName}`)
+  .then(() => console.log('Connection Ok'))
+  .catch(err => console.log('err', err));
+
+module.exports.Message = require('./message');
