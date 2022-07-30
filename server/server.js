616b8525f31b86d045ca9ae73791c724ecb77853
Viktoriia <pecourses>
2022-07-30 18:10:42 +0300

    Add message handling by ws server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const { Server } = require('socket.io');
 const app = require('./app');
+const { Message } = require('./models');
 const { all } = require('./router');
 
 const PORT = process.env.PORT ?? 5000;
@@ -19,13 +20,21 @@ const io = new Server(httpServer, {
   cors,
 });
 
+const SOCKET_EVENTS = {
+  NEW_MESSAGE: 'NEW_MESSAGE',
+  NEW_MESSAGE_ERROR: 'NEW_MESSAGE_ERROR',
+};
+
 io.on('connection', socket => {
-  socket.on('send_message', data => {
-    console.log('data', data);
-    // save to db
-  });
-  io.emit('hi_from_server', {
-    data: 'Message from server',
+  socket.on(SOCKET_EVENTS.NEW_MESSAGE, async message => {
+    try {
+      const createdMessage = await Message.create(message);
+      // отправка сообщения всем
+      io.emit(SOCKET_EVENTS.NEW_MESSAGE, createdMessage);
+    } catch (err) {
+      // отправка сообщения себе, что произошла ошибка
+      socket.emit(SOCKET_EVENTS.NEW_MESSAGE_ERROR, err);
+    }
   });
 });
 
