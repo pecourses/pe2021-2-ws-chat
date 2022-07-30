c2fcdefbd6a60506e7b8c17ea1ffda6c9109622f
Viktoriia <pecourses>
2022-07-30 17:05:58 +0300

    Add ws client exe

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './store';
+import { io } from 'socket.io-client';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,3 +14,12 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+// ---------------------------
+const socket = io('http://localhost:5000');
+
+socket.emit('send_message', { message: 'HI)))' });
+
+socket.on('hi_from_server', data => {
+  console.log('data', data);
+});
